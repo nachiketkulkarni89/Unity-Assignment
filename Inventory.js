f31c22791f72059f73a33a0f555af05095589064
nachiketkulkarni89 <nachiket.nr@gmail.com>
2014-11-29 22:00:41 -0500

    Create Inventory.js

---
+++ Inventory.js
@@ -0,0 +1,61 @@
+#pragma strict
+
+
+var inventoryStyle : GUIStyle;
+private var isPaused : boolean = false;
+
+/*--------------
+
+KEVIN 
+
+To add a new object to be picked up int the game, place it int the world, give it a box collider and
+add the itemPickUp script, assinging it a name in the property inspector, then add it to the inventory array bellow.
+Finally, add an propriate GUI label at the bottom.
+
+---------------*/
+
+
+public var inventory = new Dictionary.<String,int>();
+inventory["Ball"] = 1;
+inventory["axe"] = 0;
+inventory["bow"] = 0;
+inventory["knife"] = 0;
+inventory["something else"] = 0;
+
+
+
+
+function Update() {
+	if(Input.GetKeyDown(KeyCode.P)) {
+		if(isPaused == false) {
+		isPaused = true;
+	} else {
+		isPaused = false;
+	}
+	}
+}
+
+function OnGUI () {
+
+var player = GameObject.FindWithTag("Player").GetComponent(characterMain);
+
+if((isPaused == false) && (player.pHealth > 0)) {
+
+
+	var inventoryStyle = GUI.skin.GetStyle("Label");
+	inventoryStyle.alignment = TextAnchor.UpperCenter;
+	inventoryStyle.fontSize = 25;
+	
+	var invSkin : GUISkin = Resources.Load("myGUI") as GUISkin;
+	GUI.skin = invSkin;
+	
+	GUI.Label(Rect(35, 340, 120, 120), "Inventory");
+
+
+	GUI.Label(Rect(35, 370, 120, 120), "Ball: " + inventory["Ball"].ToString());
+	GUI.Label(Rect(35, 400, 120, 120), "Bows: " + inventory["bow"].ToString());
+	GUI.Label(Rect(35, 430, 120, 120), "Axes: " + inventory["axe"].ToString());
+	GUI.Label(Rect(35, 460, 120, 120), "Knives: " + inventory["knife"].ToString());
+
+	}
+}
